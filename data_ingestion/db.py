6eaaba1408154a60eea66bffd5a61d65ee812e3d
Kayvan Shah <kayvan.shah@usc.edu>
2024-04-08 19:29:28 -0700

    initial commit

---
+++ data_ingestion/db.py
@@ -0,0 +1,68 @@
+from typing import Any
+
+from bson import ObjectId
+from pydantic_core import core_schema
+from pymongo import MongoClient
+from settings import config, get_logger
+
+logger = get_logger(__name__)
+
+
+# BSON and JSON compatibility addressed here
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    # @classmethod
+    # def __modify_schema__(cls, field_schema):
+    #     field_schema.update(type="string")
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, _source_type: Any, _handler: Any) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema(
+                [
+                    core_schema.is_instance_schema(ObjectId),
+                    core_schema.chain_schema(
+                        [
+                            core_schema.str_schema(),
+                            core_schema.no_info_plain_validator_function(cls.validate),
+                        ]
+                    ),
+                ]
+            ),
+            serialization=core_schema.plain_serializer_function_ser_schema(lambda x: str(x)),
+        )
+
+
+def get_collection(db, collection_name: str):
+    return db[collection_name]
+
+
+try:
+    client = MongoClient(config.MONGODB_URI)
+except Exception as e:
+    logger.error("Unable to connect to MongoDB client: %s" % e)
+
+# Create DB cursors
+main_db = client["yf_panels"]
+ticker_db = client["yf_stock_ticker_data"]
+
+# Get collections
+
+users_collection = get_collection(main_db, "users")
+users_collection.create_index([("username", 1)], unique=True)
+
+tickers_info_collection = get_collection(main_db, "tickers_info")
+tickers_info_collection.create_index([("ticker_code", 1)], unique=True)
+
+portfolios_collection = get_collection(main_db, "portfolios")
+portfolios_collection.create_index([("portfolio_name", 1)], unique=True)
---
+++ data_ingestion/settings.py
@@ -0,0 +1,64 @@
+import logging
+import os
+from functools import lru_cache
+from typing import Any
+
+from google.oauth2.service_account import Credentials
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from rich.logging import RichHandler
+
+
+class Path:
+    app_dir: str = os.path.dirname(os.path.abspath(__file__))
+    root_dir: str = os.path.dirname(app_dir)
+    secrets_dir: str = os.path.join(root_dir, "secrets")
+    env_file: str = os.path.join(root_dir, ".env")
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=Path.env_file, env_file_encoding="utf-8", extra="ignore")
+
+    GCLOUD_SERVICE_ACCOUNT_KEY_PATH: str = Field(default="<your-gcp-service-acc-key-filename>")
+    PROJECT_ID: str = Field(default="<your-gcp-project-id>")
+    PROJECT_LOCATION: str = Field(default="<your-gcp-project-location>")
+    CREDENTIALS: Any | Credentials = Field(default=None)
+
+    MONGODB_URI: str = Field(default="<mongodb-connection-string>")
+
+    OPENAI_API_KEY: str = Field(default="<your-openai-api-key>")
+    HUGGINGFACEHUB_API_TOKEN: str = Field(default="<your-huggingfacehub-access-token>")
+
+
+def get_logger(name):
+    # Create a logger
+    logger = logging.getLogger(name)
+
+    # Set the logging level (adjust as needed)
+    logger.setLevel(logging.DEBUG)
+
+    # Create a console handler and set the level
+    ch = RichHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # Create a formatter and add it to the handler
+    formatter = logging.Formatter("%(message)s")
+    ch.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(ch)
+    return logger
+
+
+@lru_cache(maxsize=64)
+def get_settings() -> Settings:
+    settings = Settings()
+    settings.CREDENTIALS = Credentials.from_service_account_file(
+        os.path.join(Path.secrets_dir, settings.GCLOUD_SERVICE_ACCOUNT_KEY_PATH),
+        scopes=["https://www.googleapis.com/auth/cloud-platform"],
+    )
+    return settings
+
+
+config = get_settings()
+# print(config.model_dump())
