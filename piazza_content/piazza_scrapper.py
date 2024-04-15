71e7221ba16c045dfac6677f273be9f4cf8aa20c
sprathip <134804087+sprathip@users.noreply.github.com>
2024-04-14 19:45:53 -0700

    Update piazza_scrapper.py

--- piazza_content/piazza_scrapper.py
+++ piazza_content/piazza_scrapper.py
@@ -1,29 +1,41 @@
+import csv
 import json
 import sys
 import time
 import random
+import re
 
 from piazza_api import Piazza
 from piazza_api.exceptions import RequestError
 
 p = Piazza()
 p.user_login()
-
-course = p.network(sys.argv[1])
-
-mapSave = {}
+course = p.network("lhjo3qc1jfn1e1")
 
 posts = course.iter_all_posts(limit=100000000000)
-for post in posts:
-    content = post["history"][0]["content"]
-    post_id = post["nr"]
-    print(post_id)
-    mapSave[post_id] = content
-    time.sleep(1)
-    try:
-        with open("DSCI552.json", "w") as f:
-            f.write(json.dumps(mapSave))
-    except Exception as e:
-        print("Error:", e)
+
+# Define column names for the CSV file
+fieldnames = ['Post ID', 'Post Created Date','Post Title', 'Folder Name', 'Post Content']
+
+with open("DSCI552.csv", "w", newline='', encoding='utf-8') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for post in posts:
+        
+        post_id = post["nr"]
+        post_title = post["history"][0]["subject"]
+        folder_name = post["folders"]
+        post_created_date=post["history"][0]["created"]
+        content = post["history"][0]["content"]
+        content = content.replace('\n', ' ')
+        post_content = re.sub(r'<[^>]*>', '', content)
+        writer.writerow({'Post ID': post_id, 
+                         'Post Created Date':post_created_date,
+                         'Post Title': post_title,
+                         'Folder Name': folder_name, 
+                         'Post Content': post_content})
+
+        time.sleep(1)
 
 print("Data saved successfully!")
