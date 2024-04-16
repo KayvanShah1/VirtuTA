64412bd1cd78b5ef82a1ec7fa86651f3a885ac2c
sprathip <134804087+sprathip@users.noreply.github.com>
2024-04-15 19:56:36 -0700

    Update piazza_scrapper.py

--- piazza_content/piazza_scrapper.py
+++ piazza_content/piazza_scrapper.py
@@ -1,42 +1,65 @@
 import csv
-import re
+import json
+import sys
 import time
+import random
+import re
 
 from piazza_api import Piazza
+from piazza_api.exceptions import RequestError
+def scraping(course_id,path_to_csv):
+    p = Piazza()
+    p.user_login()
+    course = p.network(course_id)
 
-p = Piazza()
-p.user_login()
-# course = p.network("lhjo3qc1jfn1e1")
-course = p.network("loendg1jxkryr")
+    posts = course.iter_all_posts(limit=1000)
+    fieldnames = ['Post ID', 'Post Created Date', 'Post Title', 'Folder Name', 'Post Content', 'Child Content']
 
-posts = course.iter_all_posts(limit=100000000000)
+    with open(path_to_csv, "w", newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-# Define column names for the CSV file
-fieldnames = ["Post ID", "Post Created Date", "Post Title", "Folder Name", "Post Content"]
+        for post in posts:
+            
+            post_id = post["nr"]
+            post_title = post["history"][0]["subject"]
+            folder_name = post["folders"]
+            post_created_date = post["history"][0]["created"]
+            content = post["history"][0]["content"]
+            content = content.replace('\n', ' ')
+            post_content = re.sub(r'<[^>]*>', '', content)
+            has_children = bool(post.get("children"))
+            child_content_combined = {}
+            if has_children:
+                children = post["children"]
+                for idx, child in enumerate(children, 1):
+                    child_content = {}
+                    child_content_values = {}
+                    child_content_values["type"] = child["type"]
+                    child_content_values["content"] = child.get("history", [{}])[0].get("content", "")
+                    child_content_values["content"]= re.sub(r'<[^>]*>', '', child_content_values["content"])
+                    if not child_content_values["content"]:
+                        child_content_values["content"] = child.get("subject", "")
+                        child_content_values["content"]= re.sub(r'<[^>]*>', '', child_content_values["content"])
+                
+                    child_content[f"Child {idx}"] = child_content_values
+                    if child.get("children"):
+                        for nested_child_idx, nested_child in enumerate(child["children"], 1):
+                            nested_child_content_values = {}
+                            nested_child_content_values["type"] = nested_child["type"]
+                            nested_child_content_values["content"] = re.sub(r'<[^>]*>', '', nested_child.get("subject", ""))
+                            child_content[f"Nested Child {nested_child_idx}"] = nested_child_content_values
+                    
 
-with open("CSCI566.csv", "w", newline="", encoding="utf-8") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+                    child_content_combined.update(child_content)
 
-    for post in posts:
-        post_id = post["nr"]
-        print(post_id)
-        post_title = post["history"][0]["subject"]
-        folder_name = post["folders"]
-        post_created_date = post["history"][0]["created"]
-        content = post["history"][0]["content"]
-        content = content.replace("\n", " ")
-        post_content = re.sub(r"<[^>]*>", "", content)
-        writer.writerow(
-            {
-                "Post ID": post_id,
-                "Post Created Date": post_created_date,
-                "Post Title": post_title,
-                "Folder Name": folder_name,
-                "Post Content": post_content,
-            }
-        )
-
-        time.sleep(1)
-
-print("Data saved successfully!")
+            writer.writerow({'Post ID': post_id, 
+                            'Post Created Date': post_created_date,
+                            'Post Title': post_title,
+                            'Folder Name': folder_name, 
+                            'Post Content': post_content,
+                            'Child Content': json.dumps(child_content_combined)})
+            
+            time.sleep(1)
+    print("Data saved successfully!")
+scraping(sys.argv[1],sys.argv[2])
